19383d0990d5395418cc2efa50215dfb63c481b3
tanzhizz <tanzhizhen1982@gmail.com>
2022-09-18 01:48:19 +0800

    Fix cache bug.

--- app/api/client.js
+++ app/api/client.js
@@ -6,8 +6,8 @@ const apiClient = create({
 });
 
 const get = apiClient.get;
-apiClient.get = async (url, params, axiosXonfig) => {
-  const response = await get(url, params, axiosXonfig);
+apiClient.get = async (url, params, axiosConfig) => {
+  const response = await get(url, params, axiosConfig);
 
   if (response.ok) {
     cache.store(url, response.data);
