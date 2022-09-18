c2bf2c2c98b2c8e75d64b2d41abf8b2cb490d717
tanzhizz <tanzhizhen1982@gmail.com>
2022-09-18 13:28:24 +0800

    Install jwt-decode and Extracting the User from the AuthToken.

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { StyleSheet, Image } from "react-native";
 import * as Yup from "yup";
+import jwtDecode from "jwt-decode";
 
 import Screen from "../components/Screen";
 import {
@@ -28,7 +29,9 @@ function LoginScreen(props) {
     if (!result.ok) return setLoginFailed(true);
 
     setLoginFailed(false);
-    console.log(result.data);
+
+    const user = jwtDecode(result.data);
+    console.log(user);
   };
 
   return (
