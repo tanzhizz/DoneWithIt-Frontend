73654642ff91cb60e72553766e8d91212bbfff84
tanzhizz <tanzhizhen1982@gmail.com>
2022-09-20 00:36:41 +0800

    Controlling the Splash Screen.

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import jwtDecode from "jwt-decode";
+import AppLoading from "expo-app-loading";
+import * as SplashScreen from "expo-splash-screen";
 
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
@@ -12,6 +14,7 @@ import authStorage from "./app/auth/storage";
 
 export default function App() {
   const [user, setUser] = useState();
+  const [isReady, setIsReady] = useState(false);
 
   const restoreToken = async () => {
     const token = await authStorage.getToken();
@@ -20,9 +23,19 @@ export default function App() {
     setUser(jwtDecode(token));
   };
 
-  useEffect(() => {
-    restoreToken();
-  }, []);
+  if (!isReady) {
+    return (
+      <AppLoading
+        startAsync={restoreToken}
+        onFinish={() => setIsReady(true)}
+        onError={console.warn}
+      />
+    );
+  }
+
+  // useEffect(() => {
+  //   restoreToken();
+  // }, []);
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
@@ -33,3 +46,57 @@ export default function App() {
     </AuthContext.Provider>
   );
 }
+
+//Another way of using SplashScreen,have some errors
+// export default function App() {
+//   const [user, setUser] = useState();
+
+//   const [appIsReady, setAppIsReady] = useState(false);
+
+//   const restoreUser = async () => {
+//     const user = await authStorage.getUser();
+//     if (user) {
+//       setUser(user);
+//     }
+//     return user;
+//   };
+
+//   useEffect(() => {
+//     async function prepare() {
+//       try {
+//         await SplashScreen.preventAutoHideAsync();
+//         const result = await restoreUser();
+//       } catch (e) {
+//         console.warn(e);
+//       } finally {
+//         setAppIsReady(true);
+//       }
+//     }
+
+//     prepare();
+//   }, []);
+
+//   const onLayoutRootView = useCallback(async () => {
+//     if (appIsReady) {
+//       const result = await SplashScreen.hideAsync();
+//       // console.log(result, 'hide?');
+//     }
+//   }, [appIsReady]);
+
+//   if (!appIsReady) {
+//     return null;
+//   }
+
+//   return (
+//     <AuthContext.Provider value={{ user, setUser }}>
+//       <OfflineNotice />
+//       <NavigationContainer
+//         ref={navigationRef}
+//         theme={navigationTheme}
+//         onReady={onLayoutRootView}
+//       >
+//         {user ? <AppNavigator /> : <AuthNavigator />}
+//       </NavigationContainer>
+//     </AuthContext.Provider>
+//   );
+// }
