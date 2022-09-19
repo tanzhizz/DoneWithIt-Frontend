2a12b5c122e48239f4abd3ade72b7c47d680b285
tanzhizz <tanzhizhen1982@gmail.com>
2022-09-20 00:55:43 +0800

    Controlling the Splash Screen use expo-splash-screen.

--- App.js
+++ App.js
@@ -12,9 +12,48 @@ import AuthNavigator from "./app/navigation/AuthNavigator";
 import AuthContext from "./app/auth/context";
 import authStorage from "./app/auth/storage";
 
+// export default function App() {
+//   const [user, setUser] = useState();
+//   const [isReady, setIsReady] = useState(false);
+
+//   const restoreToken = async () => {
+//     const token = await authStorage.getToken();
+//     if (!token) return;
+
+//     setUser(jwtDecode(token));
+//   };
+
+//   if (!isReady) {
+//     return (
+//       <AppLoading
+//         startAsync={restoreToken}
+//         onFinish={() => setIsReady(true)}
+//         onError={console.warn}
+//       />
+//     );
+//   }
+
+//   // useEffect(() => {
+//   //   restoreToken();
+//   // }, []);
+
+//   return (
+//     <AuthContext.Provider value={{ user, setUser }}>
+//       <OfflineNotice />
+//       <NavigationContainer theme={navigationTheme}>
+//         {user ? <AppNavigator /> : <AuthNavigator />}
+//       </NavigationContainer>
+//     </AuthContext.Provider>
+//   );
+// }
+
+//Another way of using SplashScreen,have some errors
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
   const [user, setUser] = useState();
-  const [isReady, setIsReady] = useState(false);
+
+  const [IsReady, setIsReady] = useState(false);
 
   const restoreToken = async () => {
     const token = await authStorage.getToken();
@@ -23,80 +62,38 @@ export default function App() {
     setUser(jwtDecode(token));
   };
 
-  if (!isReady) {
-    return (
-      <AppLoading
-        startAsync={restoreToken}
-        onFinish={() => setIsReady(true)}
-        onError={console.warn}
-      />
-    );
-  }
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        await restoreToken();
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setIsReady(true);
+      }
+    }
 
-  // useEffect(() => {
-  //   restoreToken();
-  // }, []);
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (IsReady) {
+      await SplashScreen.hideAsync();
+      // console.log(result, 'hide?');
+    }
+  }, [IsReady]);
+
+  if (!IsReady) {
+    return null;
+  }
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
       <OfflineNotice />
-      <NavigationContainer theme={navigationTheme}>
+      <NavigationContainer theme={navigationTheme} onReady={onLayoutRootView}>
         {user ? <AppNavigator /> : <AuthNavigator />}
       </NavigationContainer>
     </AuthContext.Provider>
   );
 }
-
-//Another way of using SplashScreen,have some errors
-// export default function App() {
-//   const [user, setUser] = useState();
-
-//   const [appIsReady, setAppIsReady] = useState(false);
-
-//   const restoreUser = async () => {
-//     const user = await authStorage.getUser();
-//     if (user) {
-//       setUser(user);
-//     }
-//     return user;
-//   };
-
-//   useEffect(() => {
-//     async function prepare() {
-//       try {
-//         await SplashScreen.preventAutoHideAsync();
-//         const result = await restoreUser();
-//       } catch (e) {
-//         console.warn(e);
-//       } finally {
-//         setAppIsReady(true);
-//       }
-//     }
-
-//     prepare();
-//   }, []);
-
-//   const onLayoutRootView = useCallback(async () => {
-//     if (appIsReady) {
-//       const result = await SplashScreen.hideAsync();
-//       // console.log(result, 'hide?');
-//     }
-//   }, [appIsReady]);
-
-//   if (!appIsReady) {
-//     return null;
-//   }
-
-//   return (
-//     <AuthContext.Provider value={{ user, setUser }}>
-//       <OfflineNotice />
-//       <NavigationContainer
-//         ref={navigationRef}
-//         theme={navigationTheme}
-//         onReady={onLayoutRootView}
-//       >
-//         {user ? <AppNavigator /> : <AuthNavigator />}
-//       </NavigationContainer>
-//     </AuthContext.Provider>
-//   );
-// }
